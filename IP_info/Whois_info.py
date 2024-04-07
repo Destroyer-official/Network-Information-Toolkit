e66b289d1e31ee4ce1a9e340407cf0f207215872
Destroyer-official <156152706+Destroyer-official@users.noreply.github.com>
2024-04-07 16:37:07 +0530

    Add files via upload

---
+++ IP_info/Whois_info.py
@@ -0,0 +1,28 @@
+import whois
+import sys
+from urllib.parse import urlparse
+
+def whois_info(url):
+    try:
+        if not url.startswith("http://") and not url.startswith("https://"):
+            url = "http://" + url  # Adding prefix if not present
+        parsed_url = urlparse(url)
+        domain = parsed_url.netloc
+        domain_info = whois.whois(domain)
+        if domain_info:
+            output_filename = f"{domain}_Whois.txt"
+            with open(output_filename, 'w') as f:
+                f.write("WHOIS information for {}\n".format(domain))
+                f.write("=" * 50 + "\n")
+                f.write(str(domain_info))
+                print("Whois information saved to:", output_filename)
+        else:
+            print("No Whois information available for", domain)
+    except Exception as e:
+        print("Error occurred:", str(e))
+
+if __name__ == "__main__":
+    url = "www.google.com"  # Example URL without prefix
+    whois_info(url)
+
+
